e90825de781a6a6c9025371ec9665f5301cb6341
Andrew-740 <crawfordandrew912@gmail.com>
2025-09-25 13:18:17 -0400

    cats

--- cat.php
+++ cat.php
@@ -1,5 +1,18 @@
 <?php
 
+$url = "https://api.thecatapi.com/v1/images/search?limit=3";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+foreach ($data as $cat) {
+    echo $cat['url'] . PHP_EOL;
+}
 
 /*
 Get a cat inage to display on the screen
--- catFull.php
+++ catFull.php
@@ -2,4 +2,5 @@
 
 /*
 Get A Image of the cat and details
+https://api.thecatapi.com/v1/images/search
 */
